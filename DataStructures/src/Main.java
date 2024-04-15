9db7e42c18189474abf3702407a1ac70510be6da
Geek001748 <arsen.serikbai01@gmail.com>
2024-04-15 11:01:12 +0500

    Initial Classes

---
+++ DataStructures/src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+        boolean go = true;
+            do {
+                System.out.println();
+                System.out.println("Write 0 to exit, and 11 to switch to hackerRank");
+                System.out.println("Which problem to show write number (1-10): ");
+                Scanner sc = new Scanner(System.in);
+                case 0:
+                    System.out.println("Bye");
+                    go = false;
+                    break;
+                case 1:
+                    break;
+            } while (go == true);
+    }
+
+        private static void Stack() {
+            boolean go = true;
+            do {
+                System.out.println();
+                System.out.println("Write 0 to exit, and 11 to switch to hackerRank");
+                System.out.println("Which problem to show write number (1-10): ");
+                Scanner sc = new Scanner(System.in);
+                case 0:
+                    System.out.println("Bye");
+                    go = false;
+                    break;
+                case 1:
+                    break;
+            } while (go == true);
+        }
+         private static void Heap() {
+            boolean go = true;
+            do {
+                System.out.println();
+                System.out.println("Write 0 to exit, and 11 to switch to hackerRank");
+                System.out.println("Which problem to show write number (1-10): ");
+                Scanner sc = new Scanner(System.in);
+                case 0:
+                    System.out.println("Bye");
+                    go = false;
+                    break;
+                case 1:
+                    break;
+            } while (go == true);
+        }
+         private static void Queue() {
+            boolean go = true;
+            do {
+                System.out.println();
+                System.out.println("Write 0 to exit, and 11 to switch to hackerRank");
+                System.out.println("Which problem to show write number (1-10): ");
+                Scanner sc = new Scanner(System.in);
+                case 0:
+                    System.out.println("Bye");
+                    go = false;
+                    break;
+                case 1:
+                    break;
+            } while (go == true);
+        }
+        public void description(){
+            System.out.println();
+        }
+}
