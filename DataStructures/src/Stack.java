79d33f226e612674032bb0c17b481ee5a27a7d99
Geek001748 <arsen.serikbai01@gmail.com>
2024-04-25 16:09:04 +0500

    Stack

--- DataStructures/src/Stack.java
+++ DataStructures/src/Stack.java
@@ -1,2 +1,36 @@
-public class Stack {
+import PhysicalDataStructure.MyLinkedList;
+
+public class Stack <T extends Comparable<T> & Iterable<T>>
+{
+    private final MyLinkedList<T> list = new MyLinkedList<T>();
+    public boolean isEmpty()
+    {
+        return list.size() == 0;
+    }
+    public int size()
+    {
+        return list.size();
+    }
+    public void push(T item)
+    {
+        list.addLast(item);
+    }
+    public T pop()
+    {
+        if (isEmpty())
+        {
+            throw new IndexOutOfBoundsException("Stack is empty!");
+        }
+        T item = list.getLast();
+        list.removeLast();
+        return item;
+    }
+    public T peek()
+    {
+        if (isEmpty())
+        {
+            throw new IndexOutOfBoundsException("Stack is empty!");
+        }
+        return list.getLast();
+    }
 }
