b8e674556f0b46ad1a97335c6ade0c93ff619de3
Geek001748 <arsen.serikbai01@gmail.com>
2024-04-25 16:03:04 +0500

    Queue

--- DataStructures/src/Queue.java
+++ DataStructures/src/Queue.java
@@ -1,2 +1,36 @@
-public class Queue {
+import PhysicalDataStructure.MyLinkedList;
+
+public class Queue<T extends Comparable<T> & Iterable<T>>
+{
+    private final MyLinkedList<T> list = new MyLinkedList<T>();
+    public boolean isEmpty()
+    {
+        return list.size() == 0;
+    }
+    public int size()
+    {
+        return list.size();
+    }
+    public void enqueue(T item)
+    {
+        list.addLast(item);
+    }
+    public T dequeue()
+    {
+        if (isEmpty())
+        {
+            throw new IndexOutOfBoundsException("Queue is empty!");
+        }
+        T item = list.getFirst();
+        list.removeFirst();
+        return item;
+    }
+    public T peek()
+    {
+        if (isEmpty())
+        {
+            throw new IndexOutOfBoundsException("Queue is empty!");
+        }
+        return list.getFirst();
+    }
 }
