00492fc075377c6bb2e926a9008b89aeec14f4bf
Geek001748 <arsen.serikbai01@gmail.com>
2024-04-25 10:52:46 +0500

    Update

--- DataStructures/src/PhysicalDataStructure/MyArrayList.java
+++ DataStructures/src/PhysicalDataStructure/MyArrayList.java
@@ -1,79 +1,79 @@
 package PhysicalDataStructure;
-
 public class MyArrayList<T extends Iterable<T>> implements MyList<T>
 {
     private Object[] array;
     private int size = 0;
-    private static int capacity = 10;
-    private static final int baseCapacity = 10;
+    private int capacity = 10;
+    private static final int BASE_CAPACITY = 10;
 
     public MyArrayList()
     {
-        array = new Object[baseCapacity];
+        array = new Object[BASE_CAPACITY];
     }
 
-    void increaseBuffer()
+    private void increaseBuffer()
     {
         capacity = 2 * capacity;
-        Object[] array2 = new Object[capacity];
-        for (int i = 0; i < size; i++) {
-            array2[i] = array[i];
+        Object[] newArray = new Object[capacity];
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = array[i];
         }
-        array = array2;
+        array = newArray;
     }
-    boolean isIndex(int n)
+
+    private boolean isValidIndex(int index)
     {
-        if (n < 0 || n >= size)
-            return false;
-        return true;
+        return index >= 0 && index < size;
     }
-    void fakeIndex()
+
+    private void checkIndex(int index)
     {
-        System.out.println("You input wrong index, please repeat in range (0 - " + size + ")");
+        if (!isValidIndex(index))
+        {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
     }
+
+    private void checkSize()
+    {
+        if (size == 0)
+        {
+            throw new IndexOutOfBoundsException("Your array is empty!");
+        }
+    }
+
     @Override
     public void add(T item)
     {
-       if (size == capacity)
-       {
-           increaseBuffer();
-       }
-       array[size++] = item;
+        if (size == capacity)
+        {
+            increaseBuffer();
+        }
+        array[size++] = item;
     }
 
     @Override
     public void set(int index, T item)
     {
-        if (!isIndex(index))
-        {
-            fakeIndex();
-        }
-        else
-        {
-            array[index] = item;
-        }
+        checkIndex(index);
+        array[index] = item;
     }
 
     @Override
     public void add(int index, T item)
     {
-       if (!isIndex(index))
+        checkIndex(index);
+        if (size == capacity)
         {
-            fakeIndex();
+            increaseBuffer();
         }
-        else if (size == capacity)
+        for (int i = size; i > index; i--)
         {
-           increaseBuffer();
+            array[i] = array[i - 1];
         }
-        else
-        {
-            for (int i = size; i > index; i--)
-            {
-                array[i] = array[i - 1];
-            }
-            array[index] = item;
-            size++;
-        }
+        array[index] = item;
+        size++;
     }
 
     @Override
@@ -85,104 +85,79 @@ public class MyArrayList<T extends Iterable<T>> implements MyList<T>
     @Override
     public void addLast(T item)
     {
-        add(item);
+        add(size, item);
     }
 
     @Override
     public T get(int index)
     {
-        if (!isIndex(index))
-        {
-            fakeIndex();
-            return null;
-        }
-        return (T)array[index];
+        checkIndex(index);
+        return (T) array[index];
     }
 
     @Override
     public T getFirst()
     {
+        checkSize();
         return get(0);
     }
 
     @Override
     public T getLast()
     {
-        if (size==0)
-            return null;
+        checkSize();
         return get(size - 1);
     }
 
     @Override
-    public void remove(int index) {
-        if (size == 0 || !isIndex(index))
+    public void remove(int index)
+    {
+        checkIndex(index);
+        for (int i = index; i < size - 1; i++)
         {
-            fakeIndex();
+            array[i] = array[i + 1];
         }
-        else
-        {
-            for (int i = index; i < size - 1; i++)
-            {
-                array[i] = array[i + 1];
-            }
-            size--;
-        }
+        array[--size] = null; // clear to let GC do its work
     }
 
     @Override
     public void removeFirst()
     {
+        checkSize();
         remove(0);
     }
 
     @Override
     public void removeLast()
     {
-        if(size == 0)
-            fakeIndex();
-        else
-            remove(size - 1);
+        checkSize();
+        remove(size - 1);
     }
 
     @Override
     public void sort()
     {
-        boolean swapped;
-        for (int i = 0; i < size - 1; i++)
+        // Simple insertion sort
+        for (int i = 1; i < size; i++)
         {
-            swapped = false;
-            for (int j = 0; j < size - i - 1; j++)
-            {
-                Comparable<T> element1 = (Comparable<T>) array[j];
-                Comparable<T> element2 = (Comparable<T>) array[j + 1];
+            Object key = array[i];
+            int j = i - 1;
 
-                // Check if we need to swap array[j] and array[j + 1]
-                if (element1.compareTo((T) element2) > 0)
-                {
-                    Object temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    swapped = true;
-                }
+            while (j >= 0 && ((Comparable) array[j]).compareTo(key) > 0)
+            {
+                array[j + 1] = array[j];
+                j--;
             }
-        // If no two elements were swapped by inner loop, then break
-        if (!swapped) {
-            break;
+            array[j + 1] = key;
         }
     }
-}
-
 
     @Override
     public int indexOf(Object object)
     {
-       for (int i = 0; i < size; i++) {
-        // Check for NullPointerException
-            if (object == null && array[i] == null)
-            {
-                return i;
-            }
-            if (object != null && object.equals(array[i]))
+        for (int i = 0; i < size; i++)
+        {
+            if (object == null ? array[i] == null : object.equals(array[i]))
             {
                 return i;
             }
@@ -193,13 +168,9 @@ public class MyArrayList<T extends Iterable<T>> implements MyList<T>
     @Override
     public int lastIndexOf(Object object)
     {
-        for (int i = size; 0 <= size; i--) {
-        // Check for NullPointerException
-            if (object == null && array[i] == null)
-            {
-                return i;
-            }
-            if (object != null && object.equals(array[i]))
+        for (int i = size - 1; i >= 0; i--)
+        {
+            if (object == null ? array[i] == null : object.equals(array[i]))
             {
                 return i;
             }
@@ -216,16 +187,22 @@ public class MyArrayList<T extends Iterable<T>> implements MyList<T>
     @Override
     public Object[] toArray()
     {
-        Object[] resultArray = new Object[size];
-        System.arraycopy(array, 0, resultArray, 0, size);
-        return resultArray;
+        Object[] newArray = new Object[size];
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = array[i];
+        }
+        return newArray;
     }
 
     @Override
     public void clear()
     {
-       array = new Object[baseCapacity];
-       size = 0;
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = null;
+        }
+        size = 0;
     }
 
     @Override
